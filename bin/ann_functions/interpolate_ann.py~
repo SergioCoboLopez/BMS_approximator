5e2387ec015d9d2d9de97154ca655256fbf76852
SergioCoboLopez <srgcobo@gmail.com>
2025-07-18 15:59:10 +0200

    18/7/25

---
+++ bin/ann_functions/interpolate_ann.py~
@@ -0,0 +1,65 @@
+import pyrenn
+import numpy as np
+import copy
+import pandas as pd
+import sys
+
+
+#CAUTION!! You need to activate a virtual environment to run this code. In your terminal type:
+#source ~/entorno/bin/activate. If you don't have a virtual environment in your computer, create
+#one with numpy 1.20 and pandas 1.2.5 so that pyrenn can work without conflicting with numpy
+#To deactivate the virtual environment type "deactivate"
+
+resolution='0.5x' #1x, 2x, 0.5x, 4e-3x
+resolutions={'0.5x':'0.02', '1x':'0.01', '2x': '0.005', '4e-3x':'0.0008' }
+
+function=sys.argv[1] #tanh, leaky_ReLU
+realization=sys.argv[2]
+sigma=sys.argv[3]
+
+#Read high resolution data to interpolate
+#----------------------------------
+input_path= '../../data/generative_data/'
+file_name_d=input_path + 'NN_function_'+function + '_NREP_10_res_' + str(resolutions[resolution]) + '_interpolation_data.csv'
+
+output_path='../../data/nns/' + resolution + '_resolution/inter_extrapolation/'
+
+
+d=pd.read_csv(file_name_d)
+d=d.drop(columns='Unnamed: 0')
+d=d[(d['x1'] >= -2.0) & (d['x1']<=2.0)]
+d=d.reset_index(drop=True)
+
+n_functions=int(d['rep'].max()) #Number of functions in dataset
+
+for n in range(n_functions + 1):
+
+    dn=d[d['rep']==n]
+    dn=dn.reset_index(drop=True)
+    #----------------------------------
+
+    #Read neural network
+    #----------------------------------
+    nn_path='../../data/nns/' + str(resolution) + '_resolution/approximation/'
+    file_name_nn=nn_path + 'NN_weights_no_overfit_' + function + '_sigma_' + str(sigma) +\
+        '_rep_' + str(n) + '_r_' + str(realization) + '.csv'
+
+    nn=pyrenn.loadNN(file_name_nn)
+    #----------------------------------
+
+    #predictions of nn
+    x_tot=dn['x1']
+    ymodel=pyrenn.NNOut(x_tot, nn)
+    
+    #Save results
+    #----------------------------------
+    try:
+        ymodels_all=np.append(ymodels_all,ymodel)
+    except NameError:
+        ymodels_all=ymodel
+    #----------------------------------
+
+#Save updated data with model
+d['ymodel']=ymodels_all
+d.to_csv( output_path + 'NN_no_overfit_' + function + '_sigma_' + str(sigma) + '_r_' + str(realization) + '_res_'  + resolutions[resolution] + '.csv')
+# d.to_csv('../data/inter_extrapolate_nns/'+ 'NN_no_overfit_' + function + '_sigma_' + str(sigma) + '_r_' + str(realization) + '_res_0.01.csv')
--- bin/nguyen_functions/interpolate_nns_nguyen.py
+++ bin/nguyen_functions/interpolate_nns_nguyen.py
@@ -7,30 +7,32 @@ import sys
 
 #CAUTION!! You need to activate a virtual environment to run this code. In your terminal type:
 #source ~/entorno/bin/activate. If you don't have a virtual environment in your computer, create
-#one with numpy version 1.20 and pandas version 1.2.5 so that pyrenn can work without conflicting with numpy.
+#one with numpy 1.20 and pandas 1.2.5 so that pyrenn can work without conflicting with numpy
 #To deactivate the virtual environment type "deactivate"
 
+resolution='1x' #1x, 2x, 0.5x, 4e-3x
+resolutions={'0.5x':'', '1x':'0.01', '2x': '', '4e-3x':'' }
 
-#function='leaky_ReLU' #tanh, leaky_ReLU
-
-realization=sys.argv[1]
+function=sys.argv[1] #1, 5, 7, 8, 10
 sigma=sys.argv[2]
+realization=sys.argv[3]
 
-#Read high res data
+#Read high resolution data to interpolate
 #----------------------------------
-input_path='../../data/generative_data/'
-file_name='nguyen_inter_extrapolation_data_all.csv'
-d=pd.read_csv(input_path + file_name)
+input_path= '../data/generative_data/'
+file_name_d=input_path + 'NN_function_'+function + '_NREP_10_res_' + str(resolutions[resolution]) + '_interpolate_data.csv'
+
+output_path='../data/inter_extrapolate_nns/'
+
+
+d=pd.read_csv(file_name_d)
 d=d.drop(columns='Unnamed: 0')
+d=d[(d['x1'] >= -2.0) & (d['x1']<=2.0)]
 d=d.reset_index(drop=True)
-print(d)
 
-id_functions=[1,5,7,8,10]
+n_functions=int(d['rep'].max()) #Number of functions in dataset
 
-print(d)
-
-for n in id_functions:
-    print(n)
+for n in range(n_functions + 1):
 
     dn=d[d['rep']==n]
     dn=dn.reset_index(drop=True)
@@ -38,19 +40,19 @@ for n in id_functions:
 
     #Read neural network
     #----------------------------------
-    nn_path='../../data/nguyen/1x_resolution/trained_nns/' 
-    file_name_nn='NN_weights_no_overfit_sigma_' + str(sigma)+ '_rep_' + str(n) + \
-                 '_r_' + str(realization) + '.csv'
+    nn_path='../data/' + str(resolution) + '_resolution/trained_nns/'
+    file_name_nn=nn_path + 'NN_weights_no_overfit_' + function + '_sigma_' + str(sigma) +\
+        '_rep_' + str(n) + '_r_' + str(realization) + '.csv'
 
-    nn=pyrenn.loadNN(nn_path + file_name_nn)
+    nn=pyrenn.loadNN(file_name_nn)
     #----------------------------------
 
     #predictions of nn
-    x_tot=dn['x']
+    x_tot=dn['x1']
+    ymodel=pyrenn.NNOut(x_tot, nn)
     
     #Save results
     #----------------------------------
-    ymodel=pyrenn.NNOut(x_tot, nn)
     try:
         ymodels_all=np.append(ymodels_all,ymodel)
     except NameError:
@@ -58,6 +60,6 @@ for n in id_functions:
     #----------------------------------
 
 #Save updated data with model
-d['zmodel']=ymodels_all
-d.to_csv('../../data/nguyen/inter_extrapolation/' + 'NN_no_overfit_inter_extrapolation_sigma_' +\
-         str(sigma) + '_r_' + str(realization) + '.csv')
+d['ymodel']=ymodels_all
+d.to_csv( output_path + 'NN_no_overfit_' + function + '_sigma_' + str(sigma) + '_r_' + str(realization) + '_res_'  + resolutions[resolution] + '.csv')
+# d.to_csv('../data/inter_extrapolate_nns/'+ 'NN_no_overfit_' + function + '_sigma_' + str(sigma) + '_r_' + str(realization) + '_res_0.01.csv')
